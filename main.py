a03c29b7c5152b8756e79afd179f67cbbcacf887
LSG Costing <81208412+John-Rivero@users.noreply.github.com>
2024-06-20 13:34:15 -0500

    update
    update

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-
-
-
 from PyQt6 import QtCore, QtGui, QtWidgets
-
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -25,11 +21,16 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+        # Connect the button click event to the print_hello method
+        self.pushButton.clicked.connect(self.print_hello)
+
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.pushButton.setText(_translate("MainWindow", "PushButton"))
 
+    def print_hello(self):
+        print("hello")
 
 if __name__ == "__main__":
     import sys
